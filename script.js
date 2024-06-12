e9164e37b1ec6f893e2f7d17da09acc811860eb0
Eljesa Krasniqi <ek61368@ubt-uni.net>
2024-06-12 16:38:51 +0200

    Scroll Animations

--- script.js
+++ script.js
@@ -74,4 +74,31 @@ document.addEventListener("DOMContentLoaded", () => {
             navlist.classList.remove("open");
         });
     }
+
+    // Scroll Animation
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show-items");
+            } else {
+                entry.target.classList.remove("show-items");
+            }
+        });
+    });
+
+    const scrollLeft = document.querySelectorAll(".scroll-left");
+    scrollLeft.forEach((el) => observer.observe(el));
+
+    const scrollRight = document.querySelectorAll(".scroll-right");
+    scrollRight.forEach((el) => observer.observe(el));
+
+    const scrollTop = document.querySelectorAll(".scroll-top");
+    scrollTop.forEach((el) => observer.observe(el));
+
+    const scrollBottom = document.querySelectorAll(".scroll-bottom");
+    scrollBottom.forEach((el) => observer.observe(el));
+
+
+    const scrollScale = document.querySelectorAll(".scroll-scale");
+    scrollScale.forEach((el) => observer.observe(el));
 });
